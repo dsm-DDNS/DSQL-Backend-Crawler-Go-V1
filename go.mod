d209a11016c7cf8d5cf8e27c896b32b390db9cd4
jinu <jinwoo794533@gmail.com>
2022-09-24 22:50:59 +0900

    add function s3 Save && get Url

--- go.mod
+++ go.mod
@@ -3,19 +3,42 @@ module golang
 go 1.18
 
 require (
-	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/aws/aws-sdk-go-v2 v1.16.16
+	github.com/aws/aws-sdk-go-v2/config v1.17.7
+	github.com/aws/aws-sdk-go-v2/credentials v1.12.20
+	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.11.33
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.27.11
+	github.com/chromedp/chromedp v0.8.1
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/jmoiron/sqlx v1.3.5
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.8 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.17 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.23 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.17 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.24 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.14 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.18 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.17 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.17 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.11.23 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.13.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.16.19 // indirect
+	github.com/aws/smithy-go v1.13.3 // indirect
 	github.com/chromedp/cdproto v0.0.0-20220503213059-d217b1e06d24 // indirect
-	github.com/chromedp/chromedp v0.8.1 // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.1.0 // indirect
-	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
 	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,18 +3,29 @@ package main
 import (
 	"bytes"
 	"context"
+	"errors"
 	"flag"
 	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/smithy-go/rand"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"path/filepath"
+	"strings"
+	"time"
+
 	"github.com/PuerkitoBio/goquery"
 	"github.com/chromedp/chromedp"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"golang.org/x/net/html"
 	"gopkg.in/yaml.v2"
-	"io/ioutil"
-	"log"
-	"strings"
-	"time"
 )
 
 var (
@@ -22,6 +33,30 @@ var (
 	dbSets DbSettings
 	db     *sqlx.DB
 )
+
+type S3Info struct {
+	AwsS3Region  string `yaml:"aws_s3_region"`
+	AwsAccessKey string `yaml:"aws_access_key"`
+	AwsSecretKey string `yaml:"aws_secret_key"`
+	BucketName   string `yaml:"bucket_name"`
+	S3Client     *s3.Client
+}
+
+var s3Client S3Info
+var uuid = rand.UUID{}
+
+func parseS3FromConfig() S3Info {
+	file, err := ioutil.ReadFile("./resources/s3.yaml")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	err = yaml.Unmarshal(file, &s3Client)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	return s3Client
+}
 
 type sel struct {
 	selection *goquery.Selection
@@ -31,14 +66,13 @@ type DbSettings struct {
 	Database struct {
 		Username     string `yaml:"username"`
 		Pw           string `yaml:"password"`
-		Addr         string `yaml:"address"`
-		Port         string `yaml:"port"`
-		DbName       string `yaml:"dbName"`
+		DbUrl        string `yaml:"url"`
+		DbName       string `yaml:"db_name"`
 		DatabaseType string `yaml:"databaseType"`
 	} `yaml:"database"`
 }
 
-func parseFromConfig() DbSettings {
+func parseDbFromConfig() DbSettings {
 	var set DbSettings
 	file, err := ioutil.ReadFile("./resources/properties.yaml")
 	if err != nil {
@@ -53,12 +87,15 @@ func parseFromConfig() DbSettings {
 }
 
 func init() {
+	parseS3FromConfig()
+	err := s3Client.SetS3ConfigByKey()
 
-	dbSets = parseFromConfig()
+	dbSets = parseDbFromConfig()
+
 	flag.StringVar(&url, "t", " ", "target to crawling")
 	flag.Parse()
 
-	dbSource := fmt.Sprintf(dbSets.Database.Username + ":" + dbSets.Database.Pw + "@(" + dbSets.Database.Addr + ":" + dbSets.Database.Port + ")/" + dbSets.Database.DbName)
+	dbSource := fmt.Sprintf(dbSets.Database.Username + ":" + dbSets.Database.Pw + "@(" + dbSets.Database.DbUrl + ")/" + dbSets.Database.DbName)
 
 	open, err := sqlx.Open(dbSets.Database.DatabaseType, dbSource)
 	err = open.Ping()
@@ -66,11 +103,17 @@ func init() {
 		log.Fatal(err.Error())
 	}
 	db = open
+
 }
 
 func main() {
 
-	defer db.Close()
+	defer func(db *sqlx.DB) {
+		err := db.Close()
+		if err != nil {
+
+		}
+	}(db)
 
 	log.Println("Target: {" + url + "}")
 
@@ -102,6 +145,9 @@ func main() {
 		chromedp.WaitVisible("se-main-container"),
 		chromedp.OuterHTML("se-main-container", &str),
 	)
+	if err != nil {
+		println("ERROR:", err)
+	}
 
 	log.Println("PARSING DATA.....")
 	doc, err := goquery.NewDocumentFromReader(strings.NewReader(str))
@@ -114,8 +160,9 @@ func main() {
 
 	//content := strings.ReplaceAll(s.GetData(), "#", "")
 	content := s.GetData()
+	img := s.getImageSrc()
 
-	InsertBlogContent(&url, &content)
+	InsertBlogContent(&url, &content, &img)
 
 	if err != nil {
 		panic(err)
@@ -123,37 +170,73 @@ func main() {
 
 }
 
-func ExistsBlogContentByUrl(url *string) bool {
-	var title string
-	sql := `SELECT title FROM post WHERE url = ?`
-	err := db.QueryRow(sql, *url).Scan(&title)
+func (s *S3Info) SetS3ConfigByKey() error {
+	creds := credentials.NewStaticCredentialsProvider(s.AwsAccessKey, s.AwsSecretKey, "")
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithCredentialsProvider(creds),
+		config.WithRegion(s.AwsS3Region),
+	)
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Printf("error: %v", err)
+		panic(err)
+		return errors.New(err.Error())
 	}
-
-	println("[EXISTS]: ", title)
-
-	return true
+	s.S3Client = s3.NewFromConfig(cfg)
+	return nil
 }
 
-func InsertBlogContent(url, content *string) {
+func (s *S3Info) UploadFile(file io.Reader, filename, preFix string) *manager.UploadOutput {
+	fileType := "image/" + findExtension(filename)
+	uploader := manager.NewUploader(s.S3Client)
+	result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
+		Bucket:      aws.String(s.BucketName),
+		Key:         aws.String(preFix + "/" + filename),
+		Body:        file,
+		ContentType: &fileType,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return result
+}
+
+func findExtension(path string) string {
+	ext := filepath.Ext(path)
+	_, result, _ := strings.Cut(ext, ".")
+	return result
+}
+
+//func ExistsBlogContentByUrl(url *string) bool {
+//	var title string
+//	sql := `SELECT title FROM POST WHERE url = ?`
+//	err := db.QueryRow(sql, *url).Scan(&title)
+//	if err != nil {
+//		log.Fatal(err.Error())
+//	}
+//
+//	println("[EXISTS]: ", title)
+//
+//	return true
+//}
+
+func InsertBlogContent(url, content, img *string) {
 	var err error
-	if len(*content) == 0 {
-		_, err = db.NamedExec(`UPDATE post SET content=null WHERE url = :url`, map[string]interface{}{
+	if len(*content+*img) == 0 {
+		_, err = db.NamedExec(`UPDATE POST SET content=null, img=null WHERE url = :url`, map[string]interface{}{
 			"url": url,
 		})
 	} else {
-		_, err = db.NamedExec(`UPDATE post SET content=:content WHERE url = :url`,
+		_, err = db.NamedExec(`UPDATE POST SET content=:content, img=:img WHERE url = :url`,
 			map[string]interface{}{
 				"url":     url,
 				"content": content,
+				"img":     img,
 			})
 	}
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	log.Println("[INSERTED]: \n", *content)
+	log.Println("[INSERTED]: \n [contents]: ", *content, "\n [img]: ", *img+"\n")
 }
 
 func (s *sel) GetData() string {
@@ -180,13 +263,6 @@ func (s *sel) GetData() string {
 		//	//	return
 		//	//}
 		//}
-		if node.Data == "img" {
-			img := node.Attr[0].Val
-			if strings.Contains(img, "w80_blur") {
-				img = strings.ReplaceAll(img, "w80_blur", "w773")
-			}
-			buf.WriteString(img + "\n")
-		}
 
 		if node.Type == html.TextNode {
 			if !strings.Contains(node.Data, "\n") {
@@ -196,6 +272,53 @@ func (s *sel) GetData() string {
 				//}
 				buf.WriteString(data + "\n")
 			}
+		}
+
+		if node.FirstChild != nil {
+			for c := node.FirstChild; c != nil; c = c.NextSibling {
+				f(c)
+			}
+		}
+	}
+
+	for _, n := range s.selection.Nodes {
+		f(n)
+	}
+
+	return buf.String()
+}
+
+func (s *sel) getImageSrc() string {
+	var buf bytes.Buffer
+
+	var f func(node *html.Node)
+
+	f = func(node *html.Node) {
+		if node.Data == "img" {
+			img := node.Attr[0].Val
+			if strings.Contains(img, "w80_blur") {
+				img = strings.ReplaceAll(img, "w80_blur", "w773")
+			}
+			res, err := http.Get(img)
+			if err != nil {
+				panic(err)
+			}
+
+			defer func(Body io.ReadCloser) {
+				err := Body.Close()
+				if err != nil {
+
+				}
+			}(res.Body)
+
+			reader, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				panic(err)
+			}
+
+			filename, _ := uuid.GetUUID()
+
+			buf.WriteString(s3Client.UploadFile(bytes.NewReader(reader), filename, "post").Location + "\n")
 		}
 
 		if node.FirstChild != nil {
